572f3e45f85d5dbed89eeb788393d0ac721de3f1
kkm5291 <kkm23125291@gmail.com>
2023-10-17 18:11:40 +0900

    영속성 컨텍스트, flush, 쓰기지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,10 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.Arrays;
 import java.util.List;
 
-
-// JPA는 Transaction 단위 안에서 꼭 작업을 해줘야만 한다
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,72 +19,19 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = em.find(Member.class, 150L); // 영속상태임
+            member.setName("AAAAA");
 
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            em.clear(); // JPA에서 더이상 관리하지 않음
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.changeTeam(team);;
-            em.persist(member);
-
-
-
-            //저장
-
-//            em.flush();
-//            em.clear();
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-
-            for (Member m : members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
-
-            // 여기서 Member는 테이블명이 아닌 객체를 의미한다.
-            // 즉, Member 전체를 가져와 라는 뜻의 쿼리가 완성된 것
-//            List<Member> result = em.createQuery("select m from Member as m where m.id > 1 order by m.id", Member.class)
-//                    .setFirstResult(0)
-//                    .setMaxResults(9)
-//                    .getResultList();
-//
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//                System.out.println("member.id = " + member.getId());
-//            }
-
-            // 회원 등록 코드
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member);
-
-            // 회원 찾기
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getName());
-
-            // 회원 이름 변경
-//            findMember.setName("HelloJPA");
-
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-
-//            Member member = em.find(Member.class, 150L);
-//            member.setName("AAAAAA");
-//
-//            em.clear();
-//            Member member2 = em.find(Member.class, 150L);
-            // GENERATE.VALUE에서 IDENTITY값의 경우에는 1차캐시에 방문하는 것이 아니라, em.persist단계에서 db에 접근해서 id값을 먼저 받아오는 방식이다
-            // 버퍼링에서 값을 가져가는게 딱히 큰 매리트가 있는것은 아니라고 함
-
-            tx.commit();
+            Member member2 = em.find(Member.class, 150L); // 영속상태임
+            tx.commit(); // 커밋할 대 아무일 도 일어나지 않암름
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
         }
         emf.close();
+
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,24 +1,20 @@
 package hellojpa;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Member {
+    public Member() {}
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "MEMBER_ID")
     private Long id;
+    private String name;
 
-    @Column(name = "USERNAME")
-    private String username;
-
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
 
     public Long getId() {
         return id;
@@ -28,46 +24,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void changeTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
-    }
-
-    //    public void setUsername(String username) {
-//        this.username = username;
-//    }
-
-//    private int age;
-
-    //    // db에서 ENUM처럼 사용할 수 있게 해주는 것
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createDate;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//
-//    @Lob
-//    private String description;
-//
-//    // 해당 필드는 테이블로 들어가지 않고 메모리상으로만 사용하겠다
-//    @Transient
-//    private int tmp;
-    public Member() {
-
+    public void setName(String name) {
+        this.name = name;
     }
 }
